b2de601ce28dec719764de8209aea4a68570d929
DanDanMa <d085770128@gmail.com>
2020-05-30 13:46:19 +0800

    Move 目視大致正常

--- code/Move.py
+++ code/Move.py
@@ -1,20 +1,41 @@
 import Player
 import cmath
 import math
+import select
+import Army
+datas = select.selectMap(1)
+map = select.constructMap(datas)
+# for i in range(len(map)):
+#     print(map[i])
+# print(map[2][2])
+a = [0,1,2,3]
 
 def countRange(armyX,armyY,distanceX,distanceY):
        range = cmath.sqrt((armyX - distanceX) * (armyX - distanceX) + (armyY - distanceY * armyY - distanceY))
        range = math.ceil(range.real)
        return range
 
-def move(player = Player(),i,x,y):## x y 是要過去的座標
-       if (1<= player.army[i].type <=10):##合法兵種
-              range = countRange(player.army[i].x, player.army[i].y, x, y)
-       else:##錯誤軍種
-              return False
-       if(range<=player.army[i].movement):##合法移動範圍
-              player.army[i].x = x ##移動軍隊
-              player.army[i].y = y
-              return True
-       else:##非法移動範圍
+def move(player,i,x,y):## x y 是要過去的座標
+       road = 0 ##定義路面數字
+       if(player.army[i].move == 0 ):##選擇的士兵尚未移動
+              if (player.army[i] != None):##傭有此軍隊
+                     if(map[x][y] == road):##是可以到達的地形
+                            range = countRange(player.army[i].x, player.army[i].y, x, y)##計算軍隊到目的地的距離
+                     else:##無法到達的地形
+                            print("無法抵達目的地")
+                            return False
+              else:##錯誤軍種
+                     print("無此軍隊")
+                     return False
+              if(range<=player.army[i].movement):##合法移動範圍
+                     player.army[i].x = x ##移動軍隊
+                     player.army[i].y = y
+                     player.army[i].move = 1
+                     print("移動成功")
+                     return True
+              else:##非法移動範圍
+                     print("超出移動範圍")
+                     return False
+       else:##軍隊已經移動了
+              print("此軍隊已經移動過")
               return False
--- code/Set.py
+++ code/Set.py
@@ -1,24 +1,17 @@
 import Player
 import Army
 import select
+import client
+import json
+datas = select.selectMap(1)
+map = select.constructMap(datas)
+for i in range(len(map)):
+    print(map[i])
 
-##下方為假設玩家可初始化軍隊位置的座標範圍
-x1 = 1
-y1 = 1
-x2 = 4
-y2 = 5
-## 1:路 2:水 3:山
-mapRangeX = 6
-mapRangeY = 6
-map = [3,3,3,3,3,3,3,
-       3,1,1,1,1,1,3,
-       3,1,2,2,2,1,3,
-       3,1,1,3,1,1,3,
-       3,1,1,3,3,1,3,
-       3,1,2,1,1,1,3,
-       3,3,3,3,3,3,3,]
+areas = json.loads(datas)[]##生成區域要判斷
+## 0:路 1:水 2:山
 
-def set(player = Player(),i,x,y):##傳入要設定的玩家，極其要設定的該軍隊，即要設定的XY座標
+def set(player ,i,x,y):##傳入要設定的玩家，極其要設定的該軍隊，即要設定的XY座標
     if(player.army[i].x == None & player.army[i].y == None):##如果玩家該軍隊尚未被設置將可以進行設置
         if(x>=x1 & x <=x2 & y >= y1 & y<= y2):##如果玩家要設定軍隊的座標再生成區域內
             if(map[x][y]==1):##確認是否生成座標是否不再水面或是山上
